abc24fd0b3861c1a6cb45b2ab8ce309e0ca4e3b5
KarolBurbanoN <161732776+KarolBurbanoN@users.noreply.github.com>
2024-03-26 19:11:24 -0500

    Avance implementación de funciones
    Funciones insertar, actualizar y eliminar dinero.
    
    Mejora estética del menú.

--- MiniProyecto-1/src/App.java
+++ MiniProyecto-1/src/App.java
@@ -9,33 +9,36 @@ public class App {
     static ArrayList <Cliente>  clientes = new ArrayList<Cliente>();
     static Scanner scanner = new Scanner(System.in);
     static Cliente c = new Cliente();
+    static int savings = 0; 
 
     static void showMainMenu() { 
         Byte opcionByte;   
         do{
-            System.out.println("Bienvenido");
-            System.out.println("Que desea hacer?");
+            System.out.println("\n--------------------------------------------");
+            System.out.println("---------------- Bienvenido ----------------");
+            System.out.println("Que desea hacer?\n");
             System.out.println("1. Registrar Cliente\r\n" + //
                             "2. Insertar dinero ahorrado\r\n" + //
                             "3. Actualizar dinero ahorrado\r\n" + //
                             "4. Eliminar dinero ahorrado\r\n" + //
                             "5. Buscar cliente por nombre y ver su ahorro\r\n" + //
                             "6. Listar todos los clientes\r\n" + //
-                            "7. Salir\r");        
+                            "7. Solicitar prestamos\r\n" + //
+                            "8. Solicitar CDT\r\n" + //   
+                            "9. Salir\r");        
+            System.out.println("--------------------------------------------");               
             System.out.print("Seleccione una opcion: ");
             opcionByte = scanner.nextByte();
+            System.out.println("--------------------------------------------\n"); 
             
                 switch(opcionByte){
                     case 1:
-                        System.out.println("\nHaz seleccionado Registrar Cliente ahorrado.\n");
                         registerClient();
                         break;
                     case 2: 
-                        System.out.println("\nHaz seleccionado insertar dinero ahorrado.\n");
                         insertMoney();
                         break;
                     case 3: 
-                        System.out.println("\nHaz seleccionado actualizar dinero ahorrado.");
                         updateMoney();
                         break;
                     case 4: 
@@ -50,7 +53,15 @@ public class App {
                         System.out.println("\nHaz seleccionado listar todos los clientes.");  
                         listClients();
                         break;
-                    case 7: 
+                    case 7:
+                        System.out.println("\nHaz seleccionado solicitar préstamo.");  
+                        requestLoan();
+                        break;
+                    case 8:
+                        System.out.println("\nHaz seleccionado solicitar CDT.");  
+                        requestCDT();
+                        break;
+                    case 9: 
                         System.out.println("\nSaliendo..."); 
                         break;
                     default:
@@ -61,20 +72,57 @@ public class App {
     }
 
     static void registerClient() {
-        
+        System.out.println("--------------------------------------------");
+        System.out.println("     Haz seleccionado Registrar Cliente");
+        System.out.println("--------------------------------------------");
+        System.out.println("Ingrese sus datos por favor...\n");
+        c.setNombre();
+        c.setCedula();
+        c.setNivelIngresos();
+        c.setFechaCreacion();
+        clientes.add(c);
     }
 
 
     static void insertMoney() {
-        
+        int money;
+        System.out.println("--------------------------------------------");    
+        System.out.println("      Haz seleccionado insertar dinero");
+        System.out.println("--------------------------------------------");    
+        System.out.print("Cuanto desea ingresar?: ");
+        money = scanner.nextInt();
+        savings += money;
+        System.out.println("\nEl dinero se ingreso correctamente");
+        System.out.println("El saldo de su cuenta de ahorros es $" + savings);
+        System.out.println("------------------------------------------\n"); 
     }
 
     static void updateMoney() {
-        
+
+        System.out.println("--------------------------------------------");    
+        System.out.println("   Haz seleccionado actualizar dinero");
+        System.out.println("--------------------------------------------");
+        System.out.println("El saldo de su cuenta de ahorros es $" + savings);
+        System.out.print("Ingrese el valor a actualizar: ");
+        savings = scanner.nextInt();
+        System.out.println("\nEl dinero se actualizo correctamente");
+        System.out.println("El saldo de su cuenta ahora es $" + savings);
+        System.out.println("------------------------------------------\n"); 
+
     }
     
     static void deleteMoney() {
-            
+        int delete = 0;
+        System.out.println("--------------------------------------------");    
+        System.out.println("      Haz seleccionado eliminar dinero");
+        System.out.println("--------------------------------------------");
+        System.out.println("El saldo de su cuenta de ahorros es $" + savings);
+        System.out.print("Ingrese el valor a eliminar de la cuenta: ");
+        delete = scanner.nextInt();
+        savings -= delete;
+        System.out.println("\nEl dinero se actualizo correctamente");
+        System.out.println("El saldo de su cuenta ahora es $" + savings);
+        System.out.println("------------------------------------------\n"); 
     }
     
     static void searchClients() {
@@ -84,6 +132,13 @@ public class App {
     static void listClients() {
             
     }
+
+    static void requestLoan() {
+            
+    }
     
+    static void requestCDT() {
+            
+    }
 
 }
