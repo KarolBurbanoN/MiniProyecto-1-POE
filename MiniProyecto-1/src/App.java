ef48bc987cf364893d63920f53a3bf2d475bd273
KarolBurbanoN <161732776+KarolBurbanoN@users.noreply.github.com>
2024-03-28 13:14:27 -0500

    Update App.java
    Se mejoró la implementación de las funciones insertar, actualizar y eliminar dinero. Ya que en el primer avance solo se modificaba la variable **savings** de manera general para todos los clientes. Así que se modifico el archivo app agregando la opción de escoger a que cliente para poder asignarle de manera individual el dinero. 
    
    También se añadió un mensaje al final de cada función para que el menú aparezca después de dar un enter. Ya que antes aparecía automáticamente terminada de ejecutar la función y resultaba un poco tedioso tener que volver hacia arriba para ver el resultado de la función.

--- MiniProyecto-1/src/App.java
+++ MiniProyecto-1/src/App.java
@@ -6,13 +6,16 @@ public class App {
     public static void main(String[] args) {
         showMainMenu();
     }
+
+
     static ArrayList <Cliente>  clientes = new ArrayList<Cliente>();
     static Scanner scanner = new Scanner(System.in);
     static Cliente c = new Cliente();
-    static int savings = 0; 
 
+    
     static void showMainMenu() { 
-        Byte opcionByte;   
+        Byte opcionByte;  
+
         do{
             System.out.println("\n--------------------------------------------");
             System.out.println("---------------- Bienvenido ----------------");
@@ -33,7 +36,7 @@ public class App {
             
                 switch(opcionByte){
                     case 1:
-                        registerClient();
+                        addClient();
                         break;
                     case 2: 
                         insertMoney();
@@ -41,17 +44,14 @@ public class App {
                     case 3: 
                         updateMoney();
                         break;
-                    case 4: 
-                        System.out.println("\nHaz seleccionado eliminar dinero ahorrado.");  
+                    case 4:  
                         deleteMoney();
                         break;
-                    case 5: 
-                        System.out.println("\nHaz seleccionado buscar cliente por nombre y ver su ahorro.");  
+                    case 5:   
                         searchClients();
                         break;
-                    case 6:
-                        System.out.println("\nHaz seleccionado listar todos los clientes.");  
-                        listClients();
+                    case 6: 
+                        listClients(clientes);
                         break;
                     case 7:
                         System.out.println("\nHaz seleccionado solicitar préstamo.");  
@@ -68,76 +68,223 @@ public class App {
                         System.out.println("Opcion incorrecta, intente de nuevo...\n"); 
                         break;
                 }
-        }while(opcionByte!=6);     
+                
+        }while(opcionByte!=9);     
     }
 
-    static void registerClient() {
-      
+
+    static void addClient() {
         System.out.println("--------------------------------------------");
         System.out.println("     Haz seleccionado Registrar Cliente");
         System.out.println("--------------------------------------------");
         System.out.println("Ingrese sus datos por favor...\n");
+
+        Cliente c = new Cliente();
         c.setNombre();
         c.setCedula();
         c.setNivelIngresos();
         c.setFechaCreacion();
         clientes.add(c);
+
+        System.out.println("--------------------------------------------");
+        System.out.println("El cliente ha sido creado exitosamente...\n");
+
+        System.out.println("Presiona Enter para volver al menú principal...");
+        scanner.nextLine(); // Espera a que el usuario presione Enter
+        scanner.nextLine(); // Consumir la nueva línea después de presionar Enter
     }
 
 
     static void insertMoney() {
-        int money;
         System.out.println("--------------------------------------------");    
         System.out.println("      Haz seleccionado insertar dinero");
-        System.out.println("--------------------------------------------");    
-        System.out.print("Cuanto desea ingresar?: ");
-        money = scanner.nextInt();
-        savings += money;
-        System.out.println("\nEl dinero se ingreso correctamente");
-        System.out.println("El saldo de su cuenta de ahorros es $" + savings);
-        System.out.println("------------------------------------------\n"); 
+        System.out.println("--------------------------------------------");
+
+        if (clientes.isEmpty()) {
+            System.out.println("No hay clientes registrados.");
+            return;
+        }
+    
+        System.out.println("Clientes registrados:");
+        
+        for (int i = 0; i < clientes.size(); i++) {
+            System.out.println((i + 1) + ". " + clientes.get(i).getNombre());
+        }
+
+        System.out.println("--------------------------------------------\n"); 
+        System.out.print("Seleccione un cliente: ");
+
+        int clienteIndex = scanner.nextInt();
+
+        if (clienteIndex < 1 || clienteIndex > clientes.size()) {
+            System.out.println("Selección inválida.");
+            return;
+        }
+        
+        System.out.print("\nCuanto desea ingresar?: ");
+        int money = scanner.nextInt();
+
+        Cliente clienteSeleccionado = clientes.get(clienteIndex - 1);
+        clienteSeleccionado.addSavings(money);
+
+        System.out.println("\nEl dinero se ingreso correctamente " + clienteSeleccionado.getNombre());
+        System.out.println("El saldo de su cuenta de ahorros es $" + clienteSeleccionado.getSavings());
+        System.out.println("--------------------------------------------\n"); 
+
+        System.out.println("Presiona Enter para volver al menú principal...");
+        scanner.nextLine(); // Espera a que el usuario presione Enter
+        scanner.nextLine(); // Consumir la nueva línea después de presionar Enter
     }
 
-    static void updateMoney() {
 
+    static void updateMoney() {
         System.out.println("--------------------------------------------");    
         System.out.println("   Haz seleccionado actualizar dinero");
         System.out.println("--------------------------------------------");
-        System.out.println("El saldo de su cuenta de ahorros es $" + savings);
+
+        if (clientes.isEmpty()) {
+            System.out.println("No hay clientes registrados.");
+            return;
+        }
+    
+        System.out.println("Clientes registrados:");
+        for (int i = 0; i < clientes.size(); i++) {
+            System.out.println((i + 1) + ". " + clientes.get(i).getNombre());
+        }
+
+        System.out.println("--------------------------------------------\n"); 
+        System.out.print("Seleccione un cliente: ");
+        int clienteIndex = scanner.nextInt();
+
+        if (clienteIndex < 1 || clienteIndex > clientes.size()) {
+            System.out.println("Selección inválida.");
+            return;
+        }
+
+        Cliente clienteSeleccionado = clientes.get(clienteIndex - 1);
+
+        System.out.println("\nEl saldo de su cuenta de ahorros es $" + clienteSeleccionado.getSavings());
         System.out.print("Ingrese el valor a actualizar: ");
-        savings = scanner.nextInt();
-        System.out.println("\nEl dinero se actualizo correctamente");
-        System.out.println("El saldo de su cuenta ahora es $" + savings);
+        int newMoney = scanner.nextInt();
+
+        clienteSeleccionado.setSavings(newMoney);
+
+        System.out.println("\nEl dinero se actualizo correctamente " + clienteSeleccionado.getNombre());
+        System.out.println("El saldo de su cuenta de ahorros es $" + clienteSeleccionado.getSavings());
         System.out.println("------------------------------------------\n"); 
 
+        System.out.println("Presiona Enter para volver al menú principal...");
+        scanner.nextLine();
+        scanner.nextLine();
     }
     
+
     static void deleteMoney() {
-        int delete = 0;
         System.out.println("--------------------------------------------");    
         System.out.println("      Haz seleccionado eliminar dinero");
         System.out.println("--------------------------------------------");
-        System.out.println("El saldo de su cuenta de ahorros es $" + savings);
+
+        if (clientes.isEmpty()) {
+            System.out.println("No hay clientes registrados.");
+            return;
+        }
+    
+        System.out.println("Clientes registrados:");
+        for (int i = 0; i < clientes.size(); i++) {
+            System.out.println((i + 1) + ". " + clientes.get(i).getNombre());
+        }
+
+        System.out.println("--------------------------------------------\n"); 
+        System.out.print("Seleccione el número del cliente: ");
+        int clienteIndex = scanner.nextInt();
+
+        if (clienteIndex < 1 || clienteIndex > clientes.size()) {
+            System.out.println("Selección inválida.");
+            return;
+        }
+
+        Cliente clienteSeleccionado = clientes.get(clienteIndex - 1);
+
+        System.out.println("\nEl saldo de su cuenta de ahorros es $" + clienteSeleccionado.getSavings());
         System.out.print("Ingrese el valor a eliminar de la cuenta: ");
-        delete = scanner.nextInt();
-        savings -= delete;
-        System.out.println("\nEl dinero se actualizo correctamente");
-        System.out.println("El saldo de su cuenta ahora es $" + savings);
-        System.out.println("------------------------------------------\n"); 
+        int delete = scanner.nextInt();
+
+        clienteSeleccionado.removeSavings(delete);
+
+        System.out.println("\nEl dinero se elimino correctamente de " + clienteSeleccionado.getNombre());
+        System.out.println("El saldo de su cuenta ahora es $" + clienteSeleccionado.getSavings());
+        System.out.println("--------------------------------------------\n");
+
+        System.out.println("\nPresiona Enter para volver al menú principal...");
+        scanner.nextLine();
+        scanner.nextLine();
     }
     
+
     static void searchClients() {
-            
+        System.out.println("--------------------------------------------");    
+        System.out.println("      Haz seleccionado buscar cliente");
+        System.out.println("--------------------------------------------");
+        System.out.print("Ingrese el nombre completo del cliente a revisar: ");
+
+        String busqueda = scanner.next();
+        ArrayList<Cliente> clienteEncontrado = searchInClass(clientes, busqueda);
+
+        if(!clienteEncontrado.isEmpty()){
+        System.out.println("\nPersonas con el nombre "+ busqueda + " encontradas :");
+            for(Cliente cliente : clienteEncontrado){
+                System.out.println("Nombre: " + cliente.getNombre());
+                System.out.println("Cedula: " + cliente.getCedula());
+                System.out.println("Fecha de Creación: " + cliente.getFechaCreacion());
+                System.out.println("Nivel de Ingresos: " + cliente.getNivelIngresos());
+                System.out.println("Saldo de Ahorro: $" + cliente.getSavings());
+            }}
+        else {System.out.println("No se ha encontrado cliente: " + busqueda);}
+
+        System.out.println("\nPresiona Enter para volver al menú principal...");
+        scanner.nextLine();
+        scanner.nextLine();
+
     }
     
-    static void listClients() {
-            
+
+    static ArrayList<Cliente> searchInClass(ArrayList<Cliente> clientes, String nombreCli){
+        ArrayList<Cliente> ClientesDB = new ArrayList<>();
+        for(Cliente cliente : clientes) {
+            if(cliente.getNombre().equals(nombreCli)){
+                ClientesDB.add(cliente);
+
+            }
+        }
+        return ClientesDB;
+
     }
+
+
+    static void listClients(ArrayList<Cliente> clientes){
+        int counter = 1;
+        System.out.println("--------------------------------------------");    
+        System.out.println("      Haz seleccionado listar clientes");
+        System.out.println("--------------------------------------------");
+        System.out.println("Los clientes son:");
+        for(Cliente cliente : clientes){
+            System.out.println("Cliente #" + counter);
+            System.out.println("Nombre: " + cliente.getNombre()); 
+
+            counter += 1;          
+        }
+
+        System.out.println("\nPresiona Enter para volver al menú principal...");
+        scanner.nextLine();
+        scanner.nextLine(); 
+    }
+
 
     static void requestLoan() {
             
     }
     
+
     static void requestCDT() {
             
     }
